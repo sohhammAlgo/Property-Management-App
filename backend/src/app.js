58e0d14c1646999d4322c97573368080febe5b90
sohhammAlgo <guravsoham61@gmail.com>
2026-06-16 15:42:57 +0530

    Set proxy

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 require('express-async-errors');
 require('dotenv').config();
 const express = require('express');
+app.set('trust proxy', 1);
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
